8e0d86cb01c2ba9eb7eb7b4abf45758ac0417920
MathijsCraft <mvdb0207@gmail.com>
2022-01-05 16:40:24 +0100

    Added stuff
    Added Meta Tags
    Added Cookie Notice
    Added Partner Popup (?partner=imber)

--- js/urlParam.js
+++ js/urlParam.js
@@ -19,6 +19,34 @@ if(coupon) {
 };
 if(percentage) {
     document.getElementById("coupon-popup-percentage").innerHTML = percentage + "%";
-}else {
+} else {
     document.getElementById("coupon-popup-percentage").innerHTML = 10 + "%";
 }
+
+
+
+// Partner Popup
+const partner = urlParams.get('partner')
+
+if(partner.toLowerCase() === 'imber') {
+    let logo = '/images/partners/imber.png';
+    let name = 'Imber Development';
+    let coupon = 'Z7LTB4Q8VS';
+    let percentage = '25'
+    let referral = 'Imber';
+
+    // Open and Close
+    document.querySelector(".partner-popup").classList.add("active")
+    document.querySelector(".partner-popup .close-btn").addEventListener("click", function(){
+        document.querySelector(".partner-popup").classList.remove("active")
+    });
+
+    // Edit Element
+    document.getElementById("partner-popup-name").innerHTML = name;
+    document.getElementById("partner-popup-logo").src = logo;
+    document.getElementById("partner-popup-percentage").innerHTML = percentage + '%';
+    document.getElementById("partner-popup-coupon").value = coupon;
+    document.getElementById("partner-popup-referral").value = referral;
+
+
+};
