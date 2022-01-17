23a4b1d68956fd1f1a314b5cb78f11a5fad6bc7c
MathijsCraft <mvdb0207@gmail.com>
2022-01-17 15:32:39 +0100

    Fixed Testimonials mobile

---
+++ js/jquery.custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            loop:true
+        },
+        600:{
+            items:2,
+            nav:false,
+            loop:true
+        },
+        1000:{
+            items:3,
+            nav:false,
+            loop:true
+        }
+    }
+  })
+
+});
